ac2e04bf6bee59e8437f63336ab298c9213b808f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-01-26 17:04:33 -0500

    Add notes on object destructuring

--- components/WelcomeScreenBubble.js
+++ components/WelcomeScreenBubble.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-
+// This syntax is object destructuring in JavaScript. It is commonly used in React components, especially in functional components or hooks, to extract specific properties from an object.
+// This is often seen in function arguments, particularly in React props.
 const WelcomeScreenBubble = ({ text, textStyle, background }) => {
     return (
         <View style={background}>
